6e114499de961325603640c2f4524e02bbf47c2e
Andy-dev1 <andre.andre.jazz@gmail.com>
2022-06-22 20:37:51 -0300

    Complex logic

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,22 +1,37 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
-interface IProps{
-    onClose:()=>void;
+interface IProps {
+    onClose: () => void;
 }
 
-const Cart = ({onClose}:IProps) => {
-    const cartItems=<ul className={classes['cart-items']}>{[{id:'c1',name:'Sushi',amout:2,price:12.99},].map((item)=><li>{item.name}</li>)}</ul>;
+const Cart = ({ onClose }: IProps) => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems=cartCtx.items.length>0;
+
+    const cartItemRemoveHandler=(id:any)=>{
+
+    };
+    const cartItemAddHandler=(item:any)=>{
+
+    };
+
+    const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item:any) => <CartItem key={item.id} price={item.price} amount={item.amount} onRemove={cartItemRemoveHandler.bind(null,item.id)} onAdd={cartItemAddHandler.bind(null,item)} name={item.name}/>)}</ul>;
 
     return <Modal onClose={onClose}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
-            <span>35.62</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={classes.actions}>
             <button className={classes['button--alt']} onClick={onClose}>Close</button>
-            <button className={classes.button}>Order</button>
+            {hasItems && <button className={classes.button}>Order</button>}
         </div>
     </Modal>
 };
