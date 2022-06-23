128959f8c9bc3792a35ab0f1dc42031c84ed17f9
Andy-dev1 <andre.andre.jazz@gmail.com>
2022-06-22 22:23:47 -0300

    Effect

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -2,22 +2,43 @@ import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCart.module.css';
 
 import CartContext from '../../store/cart-context';
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
-interface IProps{
-    onClick:()=>void;
+interface IProps {
+    onClick: () => void;
 }
 
-const HeaderCartButton = ({onClick}:IProps) => {
-    const cartCtx=useContext(CartContext);
-
-    const numberOfCartItems=cartCtx.items.reduce((curNumber,item:any)=>{
+const HeaderCartButton = ({ onClick }: IProps) => {
+    const [btnIsHightLighted, setBtnIsHightLighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
+    const numberOfCartItems = items.reduce((curNumber, item: any) => {
         return curNumber + item.amount;
-    },0);
 
-    return <button className={classes.button} onClick={onClick}>
+    }, 0);
+
+
+
+    const btnClasses = `${classes.button} ${btnIsHightLighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHightLighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHightLighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
+    return <button className={btnClasses} onClick={onClick}>
         <span className={classes.icon}>
-            <CartIcon/>
+            <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
